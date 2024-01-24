d85fbad9741ac248656d08b9632547d86458b072
Feline Huhn <feline.huhn@gmx.de>
2024-01-24 13:27:36 +0100

    resolve challenge

--- js/index.js
+++ js/index.js
@@ -1,16 +1,37 @@
 console.clear();
 
 const userElement = document.querySelector(".user");
+const errorElement = document.querySelector('[data-js="error"]');
 
 async function getUser(url) {
   const response = await fetch(url);
-  const json = await response.json();
-  return json.data;
+  if (!response.ok) {
+    console.log("Network error");
+    errorElement.textContent = "Network error";
+    userElement.textContent = "";
+    return null;
+  }
+
+  try {
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    console.error(error);
+    errorElement.textContent = "Ooops something went wrong.";
+    userElement.textContent = "";
+    return null;
+  }
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
     const user = await getUser(event.target.dataset.url);
+
+    if (!user) {
+      console.error("User not found");
+      return;
+    }
+
     userElement.innerHTML = `
     <h2>${user.first_name} ${user.last_name}</h2>
     <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
